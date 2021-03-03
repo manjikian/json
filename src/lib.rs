301d1934c0bb562d4e7bd953a04ee235f6c4db41
manjikian <hoffek@kth.se>
2021-03-04 00:22:59 +0100

    Add to_string for the Value enum

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 
 use std::collections::HashMap;
 
-enum Value {
+pub enum Value {
     Null,
     True,
     False,
@@ -27,9 +27,60 @@ enum Value {
     Object(HashMap<String, Value>),
 }
 
-enum Number {
+pub enum Number {
     Integer(isize),
     Float(f64),
+}
+
+impl Value {
+    /// Outputs a string representation of the enum `Value`.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib::{Value, Number};
+    /// use std::collections::HashMap;
+    ///
+    /// let null_value = Value::Null;
+    /// assert_eq!(null_value.to_string(), "Null".to_string());
+    ///
+    /// let true_value = Value::True;
+    /// assert_eq!(true_value.to_string(), "True".to_string());
+    ///
+    /// let false_value = Value::False;
+    /// assert_eq!(false_value.to_string(), "False".to_string());
+    ///
+    /// let string_value = Value::String(String::from("abc"));
+    /// assert_eq!(string_value.to_string(), "abc".to_string());
+    ///
+    /// let int_value = Value::Number(Number::Integer(32isize));
+    /// assert_eq! (int_value.to_string(), "32".to_string());
+    ///
+    /// let float_value = Value::Number(Number::Float(2.3));
+    /// assert_eq!(float_value.to_string(), "2.3".to_string());
+    ///
+    /// let array_value = Value::Array(vec![Value::True, Value::False]);
+    /// assert_eq!(array_value.to_string(), "[True, False]".to_string());
+    ///
+    /// let object_value = Value::Object(HashMap::new());
+    /// assert_eq!(object_value.to_string(), "{}".to_string());
+    ///
+    /// ```
+    pub fn to_string(&self) -> String {
+        match self {
+            Value::Null => { String::from("Null") }
+            Value::True => { String::from("True") }
+            Value::False => { String::from("False") }
+            Value::String(s) => { s.clone() }
+            Value::Array(v) => { String::from("dummy") }
+            Value::Object(h) => { String::from("dummy") }
+            Value::Number(n) => {
+                match n {
+                    Number::Integer(i) => { format!("{}", i) }
+                    Number::Float(f) => { format!("{}", f) }
+                }
+            }
+        }
+    }
 }
 
 pub struct Json {
